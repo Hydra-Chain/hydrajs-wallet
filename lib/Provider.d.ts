8647eb8cff92316184125bc545d3c671d9127b13
pillar-locktrip <team-locktrip@7thpillar.com>
2024-01-24 22:16:49 +0530

    removed lib folder

--- lib/Provider.d.ts
+++
@@ -1,4 +0,0 @@
-export interface IProvider {
-    rawCall: (method: string, params: any[], opts?: any) => Promise<any>;
-    cancelTokenSource: () => any;
-}
--- lib/constants.d.ts
+++
@@ -1,4 +0,0 @@
-export declare enum NetworkNames {
-    MAINNET = "hydra",
-    TESTNET = "hydra_testnet"
-}
